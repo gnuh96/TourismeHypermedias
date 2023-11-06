28698b82d0129b7eb80c527c317e2b1490b35b3a
Tuan Hung NGUYEN <ntuanhung.96@gmail.com>
2023-11-06 16:40:46 +0100

    Updates

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import {RouterProvider} from 'react-router-dom'
-import {router} from './helpers/navigation.router'
-
-function App() {
-  return <RouterProvider router={router} />
-}
-
-export default App
