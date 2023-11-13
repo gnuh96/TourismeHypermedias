27112a3ab8252884d7e0a534dabaae1679be57a4
Tuan Hung NGUYEN <ntuanhung.96@gmail.com>
2023-11-13 10:47:48 +0100

    connect omeka

---
+++ src/modules/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+  navbar: d3.select('#navbarMain'),
+  mail: 'ntuanhung.96@gmail.com',
+  apiOmk: 'http://localhost:8888/omeka-s/api/',
+  ident: 'QxZtBGKiEApx6I1qbEQMTWYlZj5Syqr5',
+  key: 'y71akXpMZh5U2oK9wCCj1TWUKWLdknjY',
+  keyGitHub: '',
+}
