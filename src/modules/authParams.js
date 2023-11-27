954af2231a1886d311197cf67cc70fa235d33692
Tuan Hung NGUYEN <ntuanhung.96@gmail.com>
2023-11-27 14:36:37 +0100

    update authParams

--- src/modules/authParams.js
+++ src/modules/authParams.js
@@ -1,7 +1,8 @@
 export const pa = {
   navbar: d3.select('#navbarMain'),
   mail: 'ntuanhung.96@gmail.com',
-  apiOmk: 'http://localhost:8888/omeka-s/api/',
+  // apiOmk: 'http://localhost:8888/omeka-s/api/',
+  apiOmk: 'http://localhost/omk_thyp_23-24_clone/api/',
   ident: 'QxZtBGKiEApx6I1qbEQMTWYlZj5Syqr5',
   key: 'y71akXpMZh5U2oK9wCCj1TWUKWLdknjY',
   keyGitHub: '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',
